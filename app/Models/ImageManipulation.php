7a6470fadc2cceaf92ae3b8ce169d8b4ff1091f5
DemondogFrancisco <francisco@demondog.es>
2022-02-14 18:05:55 +0100

    Add: ImageManipulation Model, ImageManipulationController, StoreImageManipulationRequest, UpdateImageManipulationRequest only skeleton

--- app/Models/ImageManipulation.php
+++ app/Models/ImageManipulation.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class ImageManipulation extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'path',
+        'type',
+        'data',
+        'output_path',
+        'user_id',
+        'album_id',
+        'created_at',
+        'updated_at'
+    ];
 }
